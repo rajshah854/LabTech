1cbb65c02da484403ac3255adafc0352d436d500
rajshah854 <raj.shah854@gmail.com>
2019-01-24 17:48:53 +0530

    First Commit

--- champak.sh
+++ champak.sh
@@ -2,16 +2,7 @@
 
 mkdir Lab\ Tech
 cd Lab\ Tech
-mkdir 1 2 3 4 5 6 7 8 9 10
-mkdir 11 12 13 14 15 16 17 18 19 20
-mkdir 21 22 23 24 25 26 27 28 29 30
-mkdir 31 32 33 34 35 36 37 38 39 40
-mkdir 41 42 43 44 45 46 47 48 49 50
-mkdir 51 52 53 54 55 56 57 58 59 60
-mkdir 61 62 63 64 65 66 67 68 69 70
-mkdir 71 72 73 74 75 76 77 78 79 80
-mkdir 81 82 83 84 85 86 87 88 89 90
-mkdir 91 92 93 94 95 96 97 98 99 100
+mkdir {01..100}
 nano text\ file
 
-tail -20 text\ file>text\ file\ 2
+tail -c20 text\ file>text\ file\ 2
